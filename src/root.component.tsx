cfd0a3995537aa1fa583c87a408f6fe07243fd56
Malay <hi.malay879@gmail.com>
2024-11-21 18:35:40 +0530

    Changed to functional component

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Link } from "react-router-dom";
 
 export interface Employee {
@@ -9,66 +9,58 @@ export interface Employee {
   avatar: string;
 }
 
-interface ComponentState {
-  employees: Employee[];
-}
+const Root: React.FC<{ name: string; isLoggedIn: boolean }> = ({
+  name,
+  isLoggedIn,
+}) => {
+  const [employees, setEmployees] = useState<Employee[]>([]);
 
-export default class Root extends React.Component<any, ComponentState> {
-  constructor(props: ComponentState) {
-    super(props);
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((response) => response.json())
+      .then((data) => setEmployees(data.data))
+      .catch((error) => console.error("Error fetching employees:", error));
+  }, []);
 
-    this.state = { employees: [] };
-  }
-
-  componentDidMount() {
-    fetch("https://reqres.in/api/users").then((response) => {
-      response.json().then((data) => this.setState({ employees: data.data }));
-    });
-  }
-
-  componentDidCatch(error, errorInfo) {
-    console.log(error);
-  }
-
-  render() {
-    const { employees } = this.state;
-
-    if (!employees.length) {
-      return (
-        <div className="spinner-border" role="status">
-          <span className="sr-only">Loading...</span>
-        </div>
-      );
-    }
-
+  if (!employees.length) {
     return (
-      <BrowserRouter basename="/">
-        <table className="table table-striped table-bordered table-sm">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-              <th scope="col">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {employees.map((employee: Employee) => {
-              return (
-                <tr key={employee.id}>
-                  <th>
-                  <Link to={`employees/${employee.id}`}>{employee.id}</Link>
-                  </th>
-                  <td>{employee.first_name}</td>
-                  <td>{employee.last_name}</td>
-                  <td>{employee.email}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <em>{this.props.name} using React</em>
-      </BrowserRouter>
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
     );
   }
-}
+
+  return (
+    <BrowserRouter basename="/">
+      <table className="table table-striped table-bordered table-sm">
+        <thead>
+          <tr>
+            <th scope="col">ID {isLoggedIn ? "" : "(Access Required)"}</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee: Employee) => (
+            <tr key={employee.id}>
+              <th>
+                {isLoggedIn ? (
+                  <Link to={`employees/${employee.id}`}>{employee.id}</Link>
+                ) : (
+                  ""
+                )}
+              </th>
+              <td>{employee.first_name}</td>
+              <td>{employee.last_name}</td>
+              <td>{employee.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <em>{name} using React</em>
+    </BrowserRouter>
+  );
+};
+
+export default Root;
